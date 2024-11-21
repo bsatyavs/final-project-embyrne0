b224bccc5d80b38fc391a1bd04130310f351de64
embyrne0 <eleanormargo.byrne@gmail.com>
2024-11-21 13:06:05 -0500

    updated yml file

--- project_script/file_script.R
+++ project_script/file_script.R
@@ -1,6 +1,6 @@
 # This is for setting up the project
-  # Setting the variables (acabf) from expo05 and exp07
-    # acabf: surface mass balance flux (smb)
+  # Setting the variables (lithk) from expo05 and exp07
+    # lithk: Ice thickness
     # Exp05:RCP 8.5
     # Exp07: RCP 2.6
 
