32038d5fae72635d71e0a9977eeeafe54aaa89b1
embyrne0 <eleanormargo.byrne@gmail.com>
2024-11-21 12:43:55 -0500

    script code

--- project_script/file_script.R
+++ project_script/file_script.R
@@ -1,4 +1,7 @@
 # This is for setting up the project
   # Setting the variables (acabf) from expo05 and exp07
     # acabf: surface mass balance flux (smb)
-    #
+    # Exp05:RCP 8.5
+    # Exp07: RCP 2.6
+
+
